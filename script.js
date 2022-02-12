8a752606ec370d37c7b0f9abff51b2b778e7029f
amirmkouhi <amirmkouhi1377@gmail.com>
2022-02-12 18:19:35 +0330

    JS codes with local storage system added

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const toggleButton = document.querySelector('.toggle-button')
 const body = document.querySelector('body')
 
+let theme = localStorage.getItem('theme')
+if(theme === 'dark'){
+    setDark()
+}
+
 toggleButton.addEventListener('click', () =>{
-    body.classList.toggle('dark')
-    toggleButton.classList.toggle('active')
+    // body.classList.toggle('dark')
+    // toggleButton.classList.toggle('active')
+    theme = localStorage.getItem('theme')
+    if(theme === 'dark'){
+        setLight()
+    }
+    else{
+        setDark()
+    }
 })
+
+function setDark (){
+    body.classList.add('dark')
+    toggleButton.classList.add('active')
+    localStorage.setItem('theme', 'dark')
+}
+
+function setLight(){
+    body.classList.remove('dark')
+    toggleButton.classList.remove('active')
+    localStorage.setItem('theme', 'light')
+}
